d9eb05b8d9b2e926b0f10f032e7884227e4d5da8
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-01-25 10:53:13 +0000

    Create the UserMetadataType

---
+++ content-metadata/impl/src/java/uk/ac/ox/oucs/content/metadata/model/UserMetadataType.java
@@ -0,0 +1,109 @@
+package uk.ac.ox.oucs.content.metadata.model;
+
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.user.api.UserDirectoryService;
+import org.sakaiproject.user.api.UserNotDefinedException;
+
+import java.util.Collection;
+
+/**
+ * @author Colin Hebert
+ */
+public class UserMetadataType extends MetadataType<User>
+{
+	private static UserDirectoryService userDirectoryService;
+	private static SiteService siteService;
+
+	public UserMetadataType(UserDirectoryService userDirectoryService, SiteService siteService){
+		UserMetadataType.userDirectoryService = userDirectoryService;
+		UserMetadataType.siteService = siteService;
+	}
+
+	public UserMetadataType()
+	{
+	}
+
+	public Collection<User> getAllowedValues(String siteId)
+	{
+		try
+		{
+			return userDirectoryService.getUsers(siteService.getSite(siteId).getUsers());
+		}
+		catch (IdUnusedException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public MetadataRenderer getRenderer()
+	{
+		return new UserMetadataRenderer();
+	}
+
+	@Override
+	public MetadataConverter<User> getConverter()
+	{
+		return new UserMetadataConverter();
+	}
+
+	@Override
+	public MetadataValidator<User> getValidator()
+	{
+		return new UserMetadataValidator();
+	}
+
+
+	private final class UserMetadataValidator implements MetadataValidator<User>
+	{
+		public boolean validate(User value)
+		{
+			return true;
+		}
+	}
+
+	private final class UserMetadataRenderer implements MetadataRenderer
+	{
+		public String getMetadataTypeEditTemplate()
+		{
+			return null;	//To change body of implemented methods use File | Settings | File Templates.
+		}
+
+		public String getMetadataTypePrintTemplate()
+		{
+			return null;	//To change body of implemented methods use File | Settings | File Templates.
+		}
+
+		public String getMetadataValueEditTemplate()
+		{
+			return "vm/metadata/meta_edit_user.vm";
+		}
+
+		public String getMetadataValuePrintTemplate()
+		{
+			return null;	//To change body of implemented methods use File | Settings | File Templates.
+		}
+	}
+
+	private final class UserMetadataConverter implements MetadataConverter<User>{
+
+		public String toString(User object)
+		{
+			return object.getId();
+		}
+
+		public User toObject(String string)
+		{
+			try
+			{
+				return userDirectoryService.getUser(string);
+			}
+			catch (UserNotDefinedException e)
+			{
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
